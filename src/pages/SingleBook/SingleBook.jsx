85313d10ad53e112d5004376a5aeb95bffec5de5
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-05-01 20:28:01 +0600

    the qunatity of book reduced after clicking to the delivered

--- src/pages/SingleBook/SingleBook.jsx
+++ src/pages/SingleBook/SingleBook.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const SingleBook = () => {
     const loadedBook = useLoaderData();
-    console.log(loadedBook);
+    // console.log(loadedBook);
     const { _id, name, image, description, price, quantity, supplier, sold } = loadedBook;
+    const [newQuantity, setNewQuantity] = useState(quantity);
+
+    const handleDelivered = (_id) => {
+
+        fetch(`http://localhost:5000/books/${_id}/delivered`, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            // body: JSON.stringify({})
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setNewQuantity(prevQuantity => prevQuantity - 1);
+                console.log("The value of newQuantity:", newQuantity);
+            })
+    }
+
+    // Use useEffect to log newQuantity whenever it changes
+    useEffect(() => {
+        console.log("Updated newQuantity:", newQuantity);
+    }, [newQuantity]);
+
     return (
         <div className='flex justify-center items-center my-7'>
             <div className="card card-compact w-96 bg-base-100 shadow-xl">
@@ -18,7 +42,7 @@ const SingleBook = () => {
                     <p className='text-lg font-semibold transition-colors duration-300 hover:text-emerald-700'>Quantity: {quantity}</p>
                     <p className='text-lg font-semibold transition-colors duration-300 hover:text-emerald-700'>Price: ${price}</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Delivered</button>
+                        <button onClick={() => handleDelivered(_id)} className="btn btn-primary">Delivered</button>
                     </div>
                 </div>
             </div>

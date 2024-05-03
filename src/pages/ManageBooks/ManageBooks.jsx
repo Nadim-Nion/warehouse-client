ce7944dd53e5bc3ec566dc5555b958465396ad03
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-05-03 22:53:26 +0600

    delete dunctionality implemented

--- src/pages/ManageBooks/ManageBooks.jsx
+++ src/pages/ManageBooks/ManageBooks.jsx
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 const ManageBooks = () => {
     const loadedBooks = useLoaderData();
-    // console.log(loadedBooks);
+
+    const [books, setBooks] = useState(loadedBooks);
 
     const handleDelete = _id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/books/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
 
-        fetch(`http://localhost:5000/books/${_id}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                Swal.fire({
-                    title: "Are you sure?",
-                    text: "You won't be able to revert this!",
-                    icon: "warning",
-                    showCancelButton: true,
-                    confirmButtonColor: "#3085d6",
-                    cancelButtonColor: "#d33",
-                    confirmButtonText: "Yes, delete it!"
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        /* Swal.fire({
-                        title: "Deleted!",
-                        text: "Your book has been deleted.",
-                        icon: "success"
-                        }); */
-                        // if (data.deleteCount > 0) {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
 
-                        // }
-                    }
-                });
-            })
+                            const remaining = books.filter(book => book._id !== _id);
+                            setBooks(remaining);
+                        }
+                    })
+
+            }
+        });
     }
 
     return (

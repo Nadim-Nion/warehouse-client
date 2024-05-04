72a0e5dedf3bec25968e9fdb635bd5b483b94891
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-05-04 18:15:59 +0600

    add new book form added

--- src/pages/AddItems/AddItems.jsx
+++ src/pages/AddItems/AddItems.jsx
@@ -1,48 +1,85 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const AddItems = () => {
+
+    const handleAddItem = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const price = form.price.value;
+        const email = form.email.value;
+        const image = form.image.value;
+        // console.log(name, quantity, supplier, price, email, image);
+        const newBook = { name, quantity, supplier, price, email, image };
+        console.log(newBook);
+
+        fetch('http://localhost:5000/newAddedBooks', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newBook)
+        })
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: "Congrats!!!",
+                        text: "You added a new book,",
+                        icon: "success"
+                    });
+                }
+                form.reset();
+            })
+    }
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col">
                     <h2 className='text-3xl font-bold'>Add New Item</h2>
                     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form className="card-body">
+                        <form onSubmit={handleAddItem} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Book Name</span>
                                 </label>
-                                <input type="text" placeholder="name" className="input input-bordered" required />
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Quantity</span>
                                 </label>
-                                <input type="text" placeholder="quantity" className="input input-bordered" required />
+                                <input type="text" name='quantity' placeholder="quantity" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Supplier</span>
                                 </label>
-                                <input type="text" placeholder="supplier" className="input input-bordered" required />
+                                <input type="text" name='supplier' placeholder="supplier" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Price</span>
                                 </label>
-                                <input type="text" placeholder="price" className="input input-bordered" required />
+                                <input type="text" name='price' placeholder="price" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Image URL</span>
                                 </label>
-                                <input type="text" placeholder="image" className="input input-bordered" required />
+                                <input type="text" name='image' placeholder="image" className="input input-bordered" required />
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-info">Add Item</button>
